6221d81d9a6dad3c1cbce2f1341bb1e46e26baaa
Laura Muhs <lauramuhs@Lauras-MacBook-Pro.local>
2015-09-12 11:45:01 -0500

    adding hiccup function

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -4,14 +4,19 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [hiccup.page :as page]))
 
+(defn generate-message-view
+  "This generates the HTML for displaying messages"
+  []
+  (page/html5
+   [:head
+    [:title "chatter"]]
+   [:body
+    [:h1 "Our Chat App"]]))
 (defroutes app-routes
-  (GET "/" []
-       (page/html5
-        [:head
-         [:title "chatter"]]
-        [:body
-         [:h1 "Our Chat App"]]))
+  (GET "/" [] (generate-message-view))
   (route/not-found "Not Found"))
+
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
